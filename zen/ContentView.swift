5639faa91f89a0f649101ef4847f322837d05b2d
simoneb1x <moneb1x@gmail.com>
2023-03-24 21:09:08 +0100

    first commit

--- zen/ContentView.swift
+++ zen/ContentView.swift
@@ -6,21 +6,74 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var timer: AnyCancellable?
+    @State private var timeRemaining = 1500 // 25 minuti in secondi
+    @State private var timerRunning = false
+
+    private func startTimer() {
+        timerRunning = true
+        timer = Timer.publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                } else {
+                    timer?.cancel()
+                    timerRunning = false
+                }
+            }
+    }
+
+    private func stopTimer() {
+        timerRunning = false
+        timer?.cancel()
+    }
+
+    private func resetTimer() {
+        timeRemaining = 1500
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(String(format: "%02d:%02d", timeRemaining / 60, timeRemaining % 60))
+                .font(.largeTitle)
+                .padding()
+
+            HStack {
+                Button(action: {
+                    if timerRunning {
+                        stopTimer()
+                    } else {
+                        startTimer()
+                    }
+                }) {
+                    Text(timerRunning ? "Stop" : "Start")
+                }
+                .padding()
+
+                Button(action: resetTimer) {
+                    Text("Reset")
+                }
+                .padding()
+                .disabled(timerRunning)
+            }
         }
-        .padding()
+        .frame(width: 350, height: 240)
+        .fixedSize()
+        .aspectRatio(contentMode: .fit)
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+            .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/))
+            .preferredColorScheme(.dark)
+            .environment(\.sizeCategory, .medium)
     }
 }
--- zen/zenApp.swift
+++ zen/zenApp.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 @main
-struct zenApp: App {
+struct PomodoroTimerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .background(Color.black)
+                .foregroundColor(.white)
+                .onAppear {
+                    if let window = NSApplication.shared.windows.first {
+                        window.setContentSize(NSSize(width: 350, height: 240))
+                        window.styleMask.remove(.resizable)
+                        window.titleVisibility = .hidden
+                        window.titlebarAppearsTransparent = true
+                    }
+                }
         }
     }
 }
+
