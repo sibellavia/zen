91fc6d85e525c73880bc2cb5f4bc6f13ef8db71d
simoneb1x <moneb1x@gmail.com>
2023-03-24 20:13:05 +0100

    Initial Commit

---
+++ zen/zenApp.swift
@@ -0,0 +1,17 @@
+//
+//  zenApp.swift
+//  zen
+//
+//  Created by Simone Bellavia on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct zenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
