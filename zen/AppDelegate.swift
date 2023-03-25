1ef48e5181aceac0cfe920b236dce1beff075f1c
simoneb1x <moneb1x@gmail.com>
2023-03-25 16:00:59 +0100

    added icon and fixed top-bar

--- zen/AppDelegate.swift
+++ zen/AppDelegate.swift
@@ -8,12 +8,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Create the status item and assign an image
         statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         statusItem?.button?.image = NSImage(named: "zenIcon")
+        statusItem?.button?.image?.size = NSSize(width: 16, height: 16)
+        statusItem?.length = 18 // Imposta la lunghezza della NSStatusItem
+        statusItem?.button?.imagePosition = .imageLeft // Imposta l'allineamento dell'icona
+        statusItem?.button?.font = NSFont.systemFont(ofSize: 12, weight: .regular)
+        statusItem?.button?.toolTip = "Timer"
         
         // Add a menu to the status item
         let menu = NSMenu()
         menu.addItem(withTitle: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q")
         statusItem?.menu = menu
     }
+
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // tearing down the application
@@ -23,8 +29,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 extension NSImage {
     func withProgress(_ progress: CGFloat) -> NSImage {
-        let imageWidth: CGFloat = 24
-        let imageHeight: CGFloat = 24
+        let imageWidth: CGFloat = 2
+        let imageHeight: CGFloat = 2
         let circleWidth: CGFloat = 4.0
         let circleMargin: CGFloat = 2.0
         let circleRadius = (min(imageWidth, imageHeight) - circleWidth - circleMargin) / 2.0
